5fd4b1f9dd3f48f5a7a84a1de963ade436ea7b11
Fabio Filho <fabioffer6@gmail.com>
2024-12-29 22:18:54 -0300

    Começando a movimentar as peças

--- application/program.java
+++ application/program.java
@@ -1,13 +1,12 @@
-package application;   // atualizacao
-import chess.ChessMatch;        
+package application;
+
+import Chess.ChessMatch;
+
 public class program {
-    
+
     public static void main(String[] args) {
-
         ChessMatch chessMatch = new ChessMatch();
+        
         UI.printBoard(chessMatch.getPieces());
-          
     }
-    
 }
-
